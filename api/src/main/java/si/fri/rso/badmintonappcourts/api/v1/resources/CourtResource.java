392b795aa903bdcdbee3726731d9c12b3def1129
janezbozic <janez.bozic1@gmail.com>
2021-12-20 20:22:35 +0100

    Added OpenApi

--- api/src/main/java/si/fri/rso/badmintonappcourts/api/v1/resources/CourtResource.java
+++ api/src/main/java/si/fri/rso/badmintonappcourts/api/v1/resources/CourtResource.java
@@ -1,5 +1,14 @@
 package si.fri.rso.badmintonappcourts.api.v1.resources;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import si.fri.rso.badmintonappcourts.lib.Court;
 import si.fri.rso.badmintonappcourts.services.beans.CourtBean;
 
@@ -27,6 +36,13 @@ public class CourtResource {
     @Context
     protected UriInfo uriInfo;
 
+    @Operation(description = "Get all courts in a list", summary = "Get all courts")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of courts",
+                    content = @Content(schema = @Schema(implementation = Court.class, type = SchemaType.ARRAY)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )})
     @GET
     public Response getCourts() {
         List<Court> courts = courtBean.getCourts(uriInfo);
@@ -34,9 +50,17 @@ public class CourtResource {
         return Response.status(Response.Status.OK).entity(courts).build();
     }
 
+    @Operation(description = "Get data for a court.", summary = "Get data for a court")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Court",
+                    content = @Content(
+                            schema = @Schema(implementation = Court.class))
+            )})
     @GET
     @Path("/{courtId}")
-    public Response getCourt(@PathParam("courtId") Integer courtId) {
+    public Response getCourt(@Parameter(description = "Court ID.", required = true)
+                                 @PathParam("courtId") Integer courtId) {
 
         Court cort = courtBean.getCourt(courtId);
 
@@ -47,8 +71,18 @@ public class CourtResource {
         return Response.status(Response.Status.OK).entity(cort).build();
     }
 
+    @Operation(description = "Add court.", summary = "Add court")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Court successfully added."
+            ),
+            @APIResponse(responseCode = "405", description = "Validation error .")
+    })
     @POST
-    public Response createCourt(Court cort) {
+    public Response createCourt(@RequestBody(
+            description = "DTO object with court data.",
+            required = true, content = @Content(
+            schema = @Schema(implementation = Court.class))) Court cort) {
 
         if (cort.getLocation() == null || cort.getNumber() == null ) {
             return Response.status(Response.Status.BAD_REQUEST).build();
@@ -61,9 +95,21 @@ public class CourtResource {
 
     }
 
+    @Operation(description = "Delete court.", summary = "Delete court")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Court successfully deleted."
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Not found."
+            )
+    })
     @DELETE
     @Path("{courtId}")
-    public Response deleteCourt(@PathParam("courtId") Integer courtId){
+    public Response deleteCourt(@Parameter(description = "Court ID.", required = true)
+                                    @PathParam("courtId") Integer courtId){
 
         boolean deleted = courtBean.deleteCourt(courtId);
 
@@ -75,10 +121,22 @@ public class CourtResource {
         }
     }
 
+    @Operation(description = "Update data for a court.", summary = "Update court")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Court successfully updated."
+            )
+    })
     @PUT
     @Path("{courtId}")
-    public Response putCourt(@PathParam("courtId") Integer courtId,
-                                             Court court){
+    public Response putCourt(@Parameter(description = "Court ID.", required = true)
+                                 @PathParam("courtId") Integer courtId,
+                             @RequestBody(
+                                     description = "DTO object with court data.",
+                                     required = true, content = @Content(
+                                     schema = @Schema(implementation = Court.class)))
+                                     Court court){
 
         court = courtBean.putCourt(courtId, court);
 
